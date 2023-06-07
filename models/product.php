aa1cc7ffdeb8d17ffcb2116ddd7278b4fe69ec28
Valda <valda.bimbirule1@gmail.com>
2023-06-07 10:06:24 +0300

    Final commit

--- models/product.php
+++ models/product.php
@@ -5,6 +5,11 @@ abstract class Product{
     private string $name;
     private float $price;
     private string $productType;
+    private string $size;
+    private string $weight;
+    private int $height;
+    private int $length;
+    private int $width;
 
     abstract public function setProduct($product);
 
@@ -22,6 +27,62 @@ abstract class Product{
 
     public function getProductType() {
         return $this->productType;
+    }
+
+    public function getSize() {
+        return $this->size;
+    }
+
+    public function getWeight() {
+        return $this->weight;
+    }
+
+    public function getHeight() {
+        return $this->height;
+    }
+
+    public function getLength() {
+        return $this->length;
+    }
+
+    public function getWidth() {
+        return $this->width;
+    }
+
+    public function setSku($sku) { 
+        $this->sku = $sku; 
+    }
+
+    public function setName($name) { 
+        $this->name = $name; 
+    }
+
+    public function setPrice($price) { 
+        $this->price = $price; 
+    }
+
+    public function setProductType($productType) { 
+        $this->productType = $productType; 
+    }
+
+    public function setSize($size) { 
+        $this->size = $size; 
+    }
+
+    public function setWeight($weight) { 
+        $this->weight = $weight; 
+    }
+
+    public function setHeight($height) { 
+        $this->height = $height; 
+    }
+
+    public function setLength($length) { 
+        $this->length = $length; 
+    }
+
+    public function setWidth($width) { 
+        $this->width = $width; 
     }
 
 
